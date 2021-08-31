d6a00fb22aa7bd8287311aff8fbbae82f8c88ca1
Evan Pernu <etpengineer@gmail.com>
2021-08-31 00:20:26 -0700

    scrollable menu works

--- _evan/Jellybeans/Sequencer.cpp
+++ _evan/Jellybeans/Sequencer.cpp
@@ -18,82 +18,8 @@ const FontDef font = Font_7x10;
 const int     fontWidth = 7;
 const int     fontHeight = 10;
 
-// Base class for menu item
-class MenuItem {
-  public:
-    std::string name;
-    std::string value;
-
-    MenuItem(){
-        name =  "N/A";
-        value = "N/A";
-    };
-
-    MenuItem(std::string aName, std::string aValue){
-        name = aName;
-        value = aValue;
-    };
-
-    std::string Value(){
-        return value;
-    };
-    void Increment(){};
-    void Decrement(){};
-};
-
-// Menu item whos value is an element of a list of predefined strings
-class MenuItemStr: public MenuItem {
-  public:
-    int index;
-    std::vector<std::string> values;
-
-    // aDefault must be a valid index in aValues
-    MenuItemStr(std::string aName, std::vector<std::string> aValues, int aDefault){
-        name = aName;
-        values = aValues;
-        index = aDefault;
-    };
-
-    std::string Value(){
-        return  name + "   " + values[index];
-    };
-
-    void Increment(){
-        index++;
-        index = index % values.size();
-    };
-
-    void Decrement(){
-        index--;
-        if (index < 0){
-            index = values.size() - 1;
-        }
-    };
-};
-
-// Menu item for a numeric value
-class MenuItemInt: public MenuItem {
-  public:
-    int value;
-    int max;
-    int min;
-
-    MenuItemInt(std::string aName, int aValue, int aMax, int aMin){
-        name = aName;
-        value = aValue;
-        max = aMax;
-        min = aMin;
-    };
-
-    std::string Value(){
-        return std::to_string(value);
-    };
-
-    void Increment(){};
-    void Decrement(){};
-};
-
-std::array<MenuItem, 10> menuItems;
+int  menuPos;
+bool isEditing;
 
 std::vector<std::string> allNotes {
     "A",
@@ -112,47 +38,47 @@ std::vector<std::string> allNotes {
 
 std::vector<std::string> allScales {
     "Major",
-    // "Minor"
-    // "Dorian",
-    // "Phyrgian",
-    // "Lydian",
-    // "Mixolydian",
-    // "Locrian",
+    "Minor",
+    "Dorian",
+    "Phyrgi",
+    "Lydian",
+    "Mixo",
+    "Locri",
 };
 
 std::vector<std::string> allVoicings {
     "Triad",
-    // "7th",
-    // "9th",
-    // "11th",
-    // "13th",
-    // "6th",
-    // "Sus2",
-    // "Sus4",
-    // "KB"
+    "7th",
+    "9th",
+    "11th",
+    "13th",
+    "6th",
+    "Sus2",
+    "Sus4",
+    "KennyB"
 };
 
 std::vector<std::string> allOrders {
     "Up",
-    // "Down",
-    // "Up+Down In",
-    // "Up+Down Ex",
-    // "Random"
+    "Down",
+    "U+D In",
+    "U+D Ex",
+    "Random"
 };
 
 std::vector<std::string> allRhythms {
-    "Straight",
-    // "Swing 25%",
-    // "Swing 50%",
-    // "Swing 75%",
-    // "Swing 100%"
+    "None",
+    "Sw 25%",
+    "Sw 50%",
+    "Sw 75%",
+    "Sw 100%"
 };
 
 std::vector<std::string> allInversions {
     "None",
-    // "Drop 2",
-    // "Drop 3",
-    // "Drop 4"
+    "Drop 2",
+    "Drop 3",
+    "Drop 4"
 };
 
 std::vector<std::string> allOctaves {
@@ -180,8 +106,50 @@ std::vector<std::string> allClockDivs {
     "1/64"
 };
 
-int  menuPos;
-bool isEditing;
+// Menu item whos value is an element of a list of predefined strings
+class MenuItem {
+  public:
+    int index;
+    std::string name;
+    std::string displayName; // name with spaces appended
+    std::vector<std::string> values;
+
+    MenuItem(){};
+
+    // aDefault must be a valid index in aValues
+    MenuItem(std::string aName, std::vector<std::string> aValues, int aDefault){
+        name = aName;
+        values = aValues;
+        index = aDefault;
+
+        // Each menu item's displayName needs to be padded with
+        // enough spaces to be in line with the end of the longest
+        // name, plus 1 additional space. The longest menu item name
+        // is currently "Inversion" with 10 chars.
+        displayName = aName;
+        for (unsigned int i = 0; i < 10 - name.length(); i++) {
+            displayName += " ";
+        }
+    };
+
+    std::string Value() {
+        return  displayName + values[index];
+    };
+
+    void Increment(){
+        index++;
+        index = index % values.size();
+    };
+
+    void Decrement(){
+        index--;
+        if (index < 0){
+            index = values.size() - 1;
+        }
+    };
+};
+
+std::array<MenuItem, 10> menuItems;
 
 void UpdateControls();
 void UpdateOled();
@@ -193,16 +161,16 @@ int main(void)
     patch.Init(); 
 
     // Initialize menu items
-    menuItems[0] = MenuItemStr("Tonic",     allNotes,      0);
-    menuItems[1] = MenuItemStr("Scales",    allScales,     0);
-    menuItems[2] = MenuItemStr("Division",  allClockDivs,  0);
-    menuItems[3] = MenuItemStr("Voicing",   allVoicings,   0);
-    menuItems[4] = MenuItemStr("Order",     allOrders,     0);
-    menuItems[5] = MenuItemStr("Rhythm",    allRhythms,    0);
-    menuItems[6] = MenuItemStr("Inversion", allInversions, 0);
-    menuItems[7] = MenuItemStr("Oct Rng",   allOctaves,    0);
-    menuItems[8] = MenuItemStr("Oct",       allOctaves,    0);
-    menuItems[9] = MenuItemStr("Clock In",  allClockDivs,  0);
+    menuItems[0] = MenuItem("Tonic",     allNotes,      0);
+    menuItems[1] = MenuItem("Scales",    allScales,     0);
+    menuItems[2] = MenuItem("Division",  allClockDivs,  5);
+    menuItems[3] = MenuItem("Voicing",   allVoicings,   0);
+    menuItems[4] = MenuItem("Order",     allOrders,     0);
+    menuItems[5] = MenuItem("Rhythm",    allRhythms,    0);
+    menuItems[6] = MenuItem("Inversion", allInversions, 0);
+    menuItems[7] = MenuItem("Oct Rng",   allOctaves,    5);
+    menuItems[8] = MenuItem("Oct",       allOctaves,    5);
+    menuItems[9] = MenuItem("Clock",     allClockDivs,  0);
 
     // Initialize variables
     stepNumber = 0;
@@ -240,17 +208,17 @@ void UpdateControls()
         // Update menu position
         menuPos += patch.encoder.Increment();
 
-        if (menuPos > menuItems.size() - 1)
+        if (menuPos > (int) menuItems.size() - 1)
         {
-            menuPos = menuItems.size() - 1;
-        } else if (menuPos < 0) {
+            menuPos = (int) menuItems.size() - 1;
+        } else if (menuPos <= 0) {
             menuPos = 0;
         }
 
         // menuPos = (menuPos % 10 + 10) % 10;
         // if(menuPos < 5)
         // {
-        //     isEditing = patch.encoder.RisingEdge() ? true : false;
+        isEditing = patch.encoder.RisingEdge() ? true : false;
         // }
         // else
         // {
@@ -271,7 +239,7 @@ void UpdateControls()
         } else if (inc < 0){
             menuItems[menuPos].Decrement();
         }
-        
+
         isEditing = patch.encoder.RisingEdge() ? false : true;
     }
 
@@ -295,7 +263,7 @@ void UpdateOled()
 
     // Draw debug info
     patch.display.SetCursor(0, 0);
-    std::string debug_str = std::to_string(menuPos);
+    std::string debug_str = std::to_string(menuPos) + std::to_string(isEditing);
     char* debug_cstr = &debug_str[0];
     patch.display.WriteString(debug_cstr, font, true);
 
@@ -309,10 +277,9 @@ void UpdateOled()
     patch.display.WriteString(cursor_cstr, font, true);
 
     // Draw each menu item
-    for(unsigned int i = menuPos; i < menuPos + 6; i = i + 1){
-        patch.display.SetCursor(fontWidth, i * fontHeight + 11);
-        if(i < menuItems.size())
-        {
+    for(int i = menuPos; i < menuPos + 6; i = i + 1){
+        patch.display.SetCursor(fontWidth, (i - menuPos) * fontHeight + 11);
+        if(i < (int) menuItems.size()){
             std::string str = menuItems[i].Value();
             char* cstr = &str[0];
             patch.display.WriteString(cstr, font, true);
@@ -325,10 +292,8 @@ void UpdateOled()
 
 void UpdateOutputs()
 {
-    patch.seed.dac.WriteValue(DacHandle::Channel::ONE,
-                              round((values[stepNumber] / 12.f) * 819.2f));
-    patch.seed.dac.WriteValue(DacHandle::Channel::TWO,
-                              round((values[stepNumber] / 12.f) * 819.2f));
+    patch.seed.dac.WriteValue(DacHandle::Channel::ONE, round((values[stepNumber] / 12.f) * 819.2f));
+    patch.seed.dac.WriteValue(DacHandle::Channel::TWO, round((values[stepNumber] / 12.f) * 819.2f));
 
     dsy_gpio_write(&patch.gate_output, trigOut);
     trigOut = false;
