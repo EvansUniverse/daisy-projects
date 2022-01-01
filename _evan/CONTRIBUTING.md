9a968482c0b6f32133bf5ce850d3b449a2396e0d
Evan Pernu <8094440-EvanPernu@users.noreply.gitlab.com>
2022-01-01 05:43:40 +0000

    1.3.0 - fix octave behavior

--- _evan/CONTRIBUTING.md
+++ _evan/CONTRIBUTING.md
@@ -1,9 +1,15 @@
 # Contributing
 Thank you for your interest in contributing :) Pull requests are welcome.
 
-Before merging:
-* Update `_evan/<project_name>/CHANGELOG.md` according to [Keep a Changelog's format](https://keepachangelog.com/en/1.0.0/). This project adheres to [semantic versioning](https://semver.org/).
+As you can probably tell, I'm pretty new to C++, embedded, and Daisy. I would greatly appreciate any constructive criticism or advice on my code.
+
+### Before merging
+* Update `_evan/<project_name>/CHANGELOG.md` according to [Keep a Changelog's format](https://keepachangelog.com/en/1.0.0/). 
 * Update the `VERSION` variable in `_evan/<project_name>/<project name>.cpp`.
 * Add a copy of `_evan/<project_name>/build/<project name>.bin` to `old_builds` and rename it like so: `<project name>_1-0-0.bin`.
 
-As you can probably tell, I'm pretty new to C++, embedded, and Daisy. I would greatly appreciate any constructive criticism or advice on my code :)
+### Semver
+This project adheres to [semantic versioning](https://semver.org/). Here are my definitions of the different change types:
+* Minor change (0.0.x): Doesn't change controls or behavior of the module. Bug fixes, cosmetic improvements, etc. If the user created a patch with the previous firmware, they should be able to flash the new firmware and have their patch behave exactly the same way.
+* Major change (0.x.0): Changes controls or behavior of the module.
+* Breaking change (x.0.0): Either a complete and utter overhaul of the software, or a change to the hardware.
--- _evan/lib/ev_theory/theory.cpp
+++ _evan/lib/ev_theory/theory.cpp
@@ -28,25 +28,24 @@
 
 namespace ev_theory {
     uint8_t quantizeNoteToRange(int8_t i) {
-        while (true) {
-            if (i > MAX_NOTE){
-                i -= 12;
-            } else if (i < MIN_NOTE){
-                i += 12;
-            } else {
-                break;
-            }
+        while (i > MAX_NOTE){
+            i -= SEMIS_PER_OCT;
+        }
+        while (i < MIN_NOTE){
+            i += SEMIS_PER_OCT;
         }
         return i;
+    }
+    
+    // Helper
+    uint8_t cScaleEquivalent(uint8_t note, uint8_t modeRoot){
+        return (note + SEMIS_PER_OCT - modeRoot) % SEMIS_PER_OCT;
     }
 
     uint8_t quantize(uint8_t note, std::string mode, uint8_t modeRoot){
         int base = cScaleEquivalent(note, modeRoot);
 
-        // See if that note is in the given mode
         const std::vector<int> v = modeToSemitones.at(mode);
-        //if (std::find(v.begin(), v.end(), base) == v.end()){
-
         for (uint8_t i = 0; i < v.size(); i++){
             if(v[i] == base) {
                 // If the note is diatonic, return it immediately
@@ -70,7 +69,7 @@ namespace ev_theory {
     };
 
     uint8_t noteToScaleDegree(uint8_t note, std::string mode, uint8_t modeRoot){
-        note = quantize(note, mode, modeRoot) % 12;
+        note = quantize(note, mode, modeRoot) % SEMIS_PER_OCT;
         int base = cScaleEquivalent(note, modeRoot);
         
         const std::vector<int> v = modeToSemitones.at(mode);
@@ -87,54 +86,43 @@ namespace ev_theory {
     uint8_t scaleDegreeToNote(uint8_t degree, std::string mode, uint8_t modeRoot){
         const std::vector<int>* v = &(modeToSemitones.at(mode));
         int base = v->at(degree);
-        return (base + modeRoot) % 12; 
+        return (base + modeRoot) % SEMIS_PER_OCT; 
     }
 
-    float semitoneToDac(int semi) {
-        // In Daisy Seed's DAC, 0=0v and 4095=5v. 4095/5=819, meaning 819 (dac units?)
-        // per volt or octave.
-        const float DAC_UNITS_PER_OCT = 819.2f;
-
-        // !!! HACK !!!
-        // Idk why but values of 0 are about a half a semitone out of tune. Adding an offset of 25 to nonzero
-        // values seems to fix this. I'll leave this hack here until I figure out what he issue is.
-        // relevant thread: https://forum.electro-smith.com/t/bug-found-in-daisy-examples-patch-sequencer/2159/3
-        //
-        // TODO: 25 is still a wee bit off, run more precise tests to figure out a more accurate offset.
-        if (semi == 0) {
-            return 0.f;
-        }
-        const float OFFSET = 25.f;
-    
-        return round((semi / 12.f) * DAC_UNITS_PER_OCT) + OFFSET; 
+    int16_t semitoneToDac(int16_t semi) {
+        return round(DAC_UNITS_PER_SEMI * semi); 
     }
 
-    float centsToDac(int cents) {
-        // In Daisy Seed's DAC, 0=0v and 4095=5v. 4095/5=819, meaning 819 (dac units?)
-        // per volt or octave.
-        const float DAC_UNITS_PER_SEMI = 819.2f/12.f;
-    
+    int16_t centsToDac(int16_t cents) {    
         return round(DAC_UNITS_PER_SEMI * cents * .01f); 
     }
 
+    uint16_t prepareDacValForOutput(int16_t i){
+        while (i < MIN_DAC_VALUE) {
+            i += DAC_UNITS_PER_OCT;
+        }
+        if (i == 0) {
+            return 0;
+        }
+
+        i += DAC_OFFSET_FOR_NONZERO_VALUES;
+        while (i > MAX_DAC_VALUE) {
+            i -= DAC_UNITS_PER_OCT;
+        }
+        return i;
+    };
+
     // FIXME doesn't render 0
+    // Used for debug display
     std::string floatToString(float f, uint8_t dec){
         if (f == 0.) {
             return "0";
         }
-
         for (uint8_t i = 0; i < dec; i++) {
             f = f * 10.;
         }
-
         std::string ret = std::to_string(static_cast<int>(round(f)));
         ret.insert(ret.end()-dec, 1, '.');
         return ret;
     }
 } // namespace ev_theory
-
-/* Helpers */
-
-uint8_t cScaleEquivalent(uint8_t note, uint8_t modeRoot){
-    return (note + 12 - modeRoot) % 12;
-}
